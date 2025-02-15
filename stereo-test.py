78ec6535115d9a5c9bdb31b908973b923dc344b4
esben <esben9541@gmail.com>
2025-02-15 15:05:18 +0100

    Live view of output + Gpu accelerated

--- stereo-test.py
+++ stereo-test.py
@@ -3,9 +3,9 @@ import cv2
 from matplotlib import pyplot as plt
 import time
 
+print(cv2.ocl.haveOpenCL())
 
-
-# cv2.ocl.useOpenCL()
+cv2.ocl.setUseOpenCL(True)
 cam_left = cv2.VideoCapture(4)
 cam_right = cv2.VideoCapture(6)
 
@@ -13,22 +13,23 @@ cam_right = cv2.VideoCapture(6)
 def main():
     while True:
         start_time = time.time()
-        result = ShowDisparity(bSize=15)
+        result = ShowDisparity(bSize=55)
         print(time.time() - start_time)
         print(1 / (time.time() - start_time))
+        cv2.imshow("test", result)
+        cv2.waitKey(int (1000 * (time.time() - start_time)))
 def ShowDisparity(bSize=15):
     retl, frame_left = cam_left.read()
     retr, frame_right = cam_right.read()
-    frame_left_gray = cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY)
-    frame_right_gray = cv2.cvtColor(frame_right, cv2.COLOR_BGR2GRAY)
+    frame_left_gray = cv2.UMat(cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY))
+    frame_right_gray = cv2.UMat(cv2.cvtColor(frame_right, cv2.COLOR_BGR2GRAY))
     stereo = cv2.StereoBM_create(numDisparities=32, blockSize=bSize)
-    disparity = stereo.compute(frame_left_gray, frame_right_gray)
+    disparity = stereo.compute(frame_left_gray, frame_right_gray).get()
 
-    # Normalize the image for representation
     min = disparity.min()
     max = disparity.max()
     disparity = np.uint8(255 * (disparity - min) / (max - min))
-    # Plot the result
+
     return disparity
 
 
