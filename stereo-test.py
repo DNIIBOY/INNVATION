d83875c2d36c5b69fb295e8601dcd92bb18061b2
esben <esben9541@gmail.com>
2025-02-15 14:29:03 +0100

    Added support for live cams

--- stereo-test.py
+++ stereo-test.py
@@ -3,26 +3,31 @@ import cv2
 from matplotlib import pyplot as plt
 import time
 
-imgLeft = cv2.imread('scene11.png', 0)
-imgRight = cv2.imread('scene12.png', 0)
+
+
+# cv2.ocl.useOpenCL()
+cam_left = cv2.VideoCapture(4)
+cam_right = cv2.VideoCapture(6)
+
+
 def main():
-    start_time = time.time()
-    result = ShowDisparity(bSize=15)
-    print(time.time() - start_time)
-
-
+    while True:
+        start_time = time.time()
+        result = ShowDisparity(bSize=15)
+        print(time.time() - start_time)
+        print(1 / (time.time() - start_time))
 def ShowDisparity(bSize=15):
-    # Initialize the stereo block matching object 
+    retl, frame_left = cam_left.read()
+    retr, frame_right = cam_right.read()
+    frame_left_gray = cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY)
+    frame_right_gray = cv2.cvtColor(frame_right, cv2.COLOR_BGR2GRAY)
     stereo = cv2.StereoBM_create(numDisparities=32, blockSize=bSize)
-
-    # Compute the disparity image
-    disparity = stereo.compute(imgLeft, imgRight)
+    disparity = stereo.compute(frame_left_gray, frame_right_gray)
 
     # Normalize the image for representation
     min = disparity.min()
     max = disparity.max()
     disparity = np.uint8(255 * (disparity - min) / (max - min))
-    
     # Plot the result
     return disparity
 
