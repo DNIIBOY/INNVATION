d5d470ec191881b9f2ec4c5861d1b1bfa485a74d
Boltz333 <boltz333yt@gmail.com>
2025-02-20 14:39:28 +0100

    created base for server

---
+++ innhabbitserver/occupancy/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Entrance(models.Model):
+    name = models.CharField(max_length=64)
+
+class Event(models.Model):
+    class Meta:
+        abstract = True
+    timestamp = models.DateTimeField()
+    entrance = models.ForeignKey(
+        Entrance,
+        on_delete=models.CASCADE
+    )
+
+class EntryEvent(Event):
+    entrance = models.ForeignKey(
+        Entrance,
+        on_delete=models.CASCADE,
+        related_name="entries"
+    )
+
+class ExitEvent(Event):
+    entrance = models.ForeignKey(
+        Entrance,
+        on_delete=models.CASCADE,
+        related_name="exits"
+    )
+    
